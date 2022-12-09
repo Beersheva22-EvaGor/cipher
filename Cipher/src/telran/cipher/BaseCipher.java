bc67a68fee307ec4130385dfd3982cad27000bee
eva-gor <evagor82@gmail.com>
2022-12-09 09:24:08 +0200

    HW 12a Base Cipher

---
+++ Cipher/src/telran/cipher/BaseCipher.java
@@ -0,0 +1,63 @@
+package telran.cipher;
+
+import java.util.Arrays;
+
+public class BaseCipher {
+
+	private String key;
+
+	private static final int valueMin = 33;
+	private static final int valueMax = 126;
+
+	private static final int lengthMin = 2;
+	private static final int lengthMax = 93;
+
+	public BaseCipher(int length) {
+		// creates the key as String of the random values of ASCII codes from 33 to 126
+		key = "";
+		int lengthReal = length;
+		if (length < lengthMin) {
+			lengthReal = lengthMin;
+		}
+		if (length > lengthMax) {
+			lengthReal = lengthMax;
+		}
+		int[] temparr = new int[valueMax - valueMin + 1];
+		int i = 0;
+		while (i < lengthReal) {
+			int tempnum = getRandomNumber(valueMin, valueMax);
+			if (temparr[tempnum - valueMin] != 1) {
+				key += (char) tempnum;
+				temparr[tempnum - valueMin] = 1;
+				i++;
+			}
+		}
+	}
+
+	public String cipher(int number) {
+		StringBuilder res = new StringBuilder("");		
+		int base = key.length();
+		while (number > base) {
+			res.insert(0,key.charAt(number % base));
+			number = number / base;
+		}
+		res.insert(0,key.charAt(number));
+		return res.toString();
+	}
+
+	public int decipher(String cipher) {
+		int res = 0;
+		int base = key.length();		
+		int index =0;
+		while (index<cipher.length()) {
+			res += Math.pow(base, index)*key.indexOf(cipher.charAt(cipher.length()-index-1));
+			index++;
+		}
+		return res;
+	}
+
+	private static int getRandomNumber(long min, long max) {
+		return (int) (min + Math.random() * (max - min + 1));
+	}
+
+}
---
+++ Cipher/src/telran/cipher/test/CipherTest.java
@@ -0,0 +1,41 @@
+package telran.cipher.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.cipher.BaseCipher;
+
+class CipherTest {
+
+	@Test
+	void baseCipherTest() {
+		int number = 123;		
+		BaseCipher cipherbase = new BaseCipher(2);
+		String cipher = cipherbase.cipher(number);
+		int decipher = cipherbase.decipher(cipher);
+		System.out.println("CIPHER: \t" + cipher);
+		System.out.println("DECIPHER: \t" + decipher);
+		assertEquals(number, decipher);
+		
+		cipherbase = new BaseCipher(8);		// same number - another base
+		cipher = cipherbase.cipher(number);
+		decipher = cipherbase.decipher(cipher);
+		assertEquals(number, decipher);
+		
+		// checking border conditions
+		cipherbase = new BaseCipher(0);	// left border of a key length
+		cipher = cipherbase.cipher(number);
+		decipher = cipherbase.decipher(cipher);
+		assertEquals(number, decipher);
+		
+		number = 1231231231;
+		cipherbase = new BaseCipher(100);	// right border of a key length
+		cipher = cipherbase.cipher(number);
+		decipher = cipherbase.decipher(cipher);
+		assertEquals(number, decipher);
+		
+		
+	}
+
+}
